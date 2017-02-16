4912cdbe3533c6147fbcee399715c53906167226
Michael Loschiavo <losch103@gmail.com>
2017-02-16 12:34:34 -0800

    Complete remaining challenges

---
+++ Countdown_Timer/scripts.js
@@ -0,0 +1,54 @@
+let countdown;
+const timeDisplay = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time')
+const buttons = Array.from(document.querySelectorAll('[data-time]'))
+
+function timer(seconds) {
+  clearInterval(countdown)
+
+  const now = Date.now()
+  const then = now + seconds * 1000
+
+  displayTimeLeft(seconds)
+  displayEndTime(then)
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000)
+
+    if (secondsLeft < 0) {
+      clearInterval(countdown)
+       return;
+     }
+
+    displayTimeLeft(secondsLeft)
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60)
+  const remainderSeconds = seconds % 60
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
+  document.title = display
+  timeDisplay.textContent = display
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp)
+  const hour = end.getHours()
+  const minutes = end.getMinutes()
+  endTime.textContent = `Be Back At ${hour}:${minutes}`
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time)
+  timer(seconds)
+}
+
+buttons.forEach((button) => button.addEventListener('click', startTimer))
+
+document.customForm.addEventListener('submit', function (ev) {
+  ev.preventDefault()
+  const mins = this.minutes.value
+  timer(mins * 60)
+  this.reset()
+})
